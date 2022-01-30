29b144c77fda11754be866bc5a34d8822352a3fb
jeeliarda <johneliarda@gmail.com>
2022-01-30 20:32:47 +0800

    add data structure intro

---
+++ data structure/1.0-introduction/introduction.py
@@ -0,0 +1,15 @@
+#-----------Primitive Data Types
+#int
+#float
+#boolean
+#string
+#-------------Data Structure
+#-----group and order these data types
+#list
+#tuple
+#set
+#dictionary
+#compound data structure
+#----------(last two types of) operators
+#membership
+#identity
