ebc713c0bc4eb77a99e28e30b0c60a8231dcd371
jeeliarda <johneliarda@gmail.com>
2022-01-31 21:29:57 +0800

    Compound Data Structures and Conclusion

---
+++ data structure/7.0-Compound Data Structures/Compound-Data-Structures.py
@@ -0,0 +1,17 @@
+'''Quiz: Compound Data Structures
+Q3: Check the attributes of a collection for which using a Python set would be appropriate.
+
+answer	option	reason
+(O)	Order in which items appear can be inconsistent	Sets are not ordered, so the order in which items appear can be inconsistent
+You can have the same entry multiple times	
+(O)	Mutable (you can change it)	Like dictionaries and lists, sets are mutable.
+(O)	Add items with .add()	
+Sortable	You cannot have the same item twice and you cannot sort sets. (unordered -> unsortable)
+Q4: Check the attributes of a collection for which using a Python dictionary would be appropriate.
+
+answer	option	reason
+(O)	Each item contains two parts	
+Add items with .append()	not .append(), is .add()
+(O)	Order in which items appear can be inconsistent	
+Sortable	unordered -> unsortable
+(O)	Can be nested	'''
